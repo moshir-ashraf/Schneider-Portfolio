6aa086a3c51df8b6f9c3e36b553e2c8c11b3940f
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2024-07-23 04:06:08 +0300

    perks

--- src/modules/portfolio/perks/perks.component.ts
+++ src/modules/portfolio/perks/perks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-perks',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerksComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('techList') techList!: ElementRef;
+  @ViewChild('hobbiesList') hobbiesList!: ElementRef;
+
+  private colors = ['blue', 'red', 'yellow']; // Define the color classes
+  private currentColorIndex = 0;
 
   ngOnInit() {
+    // Apply styles after the view has been initialized
+    setTimeout(() => this.applyDynamicStyles(), 0);
   }
 
+  // Function to apply styles dynamically
+  applyDynamicStyles() {
+    if (this.techList) {
+      this.applyClassToListItems(this.techList.nativeElement);
+    }
+
+    if (this.hobbiesList) {
+      this.applyClassToListItems(this.hobbiesList.nativeElement);
+    }
+  }
+
+  // Function to apply a class to list items
+  applyClassToListItems(list: HTMLElement) {
+    const items = list.getElementsByClassName('list-group-item');
+    
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i] as HTMLElement;
+      // Apply the color class and then cycle to the next one
+      item.classList.add(this.colors[this.currentColorIndex]);
+      this.currentColorIndex = (this.currentColorIndex + 1) % this.colors.length;
+    }
+  }
 }
