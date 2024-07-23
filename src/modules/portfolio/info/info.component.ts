dfd760ffaae89871abe4d4c5eab58595ed94ff4e
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2024-07-23 03:26:58 +0300

    slideshow

--- src/modules/portfolio/info/info.component.ts
+++ src/modules/portfolio/info/info.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info.component.css']
 })
 export class InfoComponent implements OnInit {
+  baseImageUrl: string = 'images/me'; // Base URL for images without 'assets' prefix
+  currentIndex: number = 0; // Starting index
+
+  // Number of images you have
+  totalImages: number = 3;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  get currentImage(): string {
+    return `${this.baseImageUrl}${this.currentIndex}.jpg`;
+  }
+
+  showPrevious(): void {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.totalImages - 1;
+  }
+
+  showNext(): void {
+    this.currentIndex = (this.currentIndex < this.totalImages - 1) ? this.currentIndex + 1 : 0;
+  }
 }
